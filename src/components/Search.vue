797bc7334703d085542c2a60fd0cbbf5f7af86d6
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2021-10-03 17:15:27 -0400

    Structuring Project

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1 class="heading grey--text py-4 px-6">{{ title }}</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+
+    data: () => ({
+      title: "iTunes"
+    }),
+  }
+</script>
